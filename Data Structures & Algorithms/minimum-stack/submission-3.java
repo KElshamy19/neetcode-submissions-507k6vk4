a85820cb0194fb19284dd0f59b6b5fa18f57c052
Kareem Elshamy <169207920+KElshamy19@users.noreply.github.com>
2026-07-31 18:44:27 +0300

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,29 @@
+class MinStack {
+    private List<Integer> stack = new ArrayList();
+    private List<Integer> min = new ArrayList();
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        stack.add(val);
+        int minVal = val;
+        if(!min.isEmpty()&&min.getLast()<val)
+            minVal=min.getLast();
+        // else minVal = val;
+        min.add(minVal);
+    }
+    
+    public void pop() {
+        stack.removeLast();
+        min.removeLast();
+    }
+    
+    public int top() {
+        return stack.getLast();
+    }
+    
+    public int getMin() {
+        return min.getLast();
+    }
+}
